7dfed559150f9c9151bb7981e2f89145a6ff088f
ismailou <ismailou@uio.no>
2024-08-21 20:14:43 +0200

    init setup

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelector('video').playbackRate = 0.8;
+
+var typed = new Typed(".multiple-text", {
+    strings:["precision", "passion", "sustainability in mind", "elegance and comfort", "unmatched quality"],
+    typeSpeed:100,
+    backpeed:100,
+    backDelay: 1000,
+    loop:true
+
+})
+
