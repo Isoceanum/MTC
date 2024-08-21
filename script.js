6ffeb73d0428325784c84e445cf7c388c5812587
ismailou <ismailou@uio.no>
2024-08-21 22:12:14 +0200

    blur background on nav bar click

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-document.querySelector('video').playbackRate = 0.8;
+const navEl = document.querySelector('.nav');
+const hamburgerEl = document.querySelector('.hamburger');
+const mainEl = document.querySelector('.main-content');
+const body = document.querySelector('body');
 
-var typed = new Typed(".multiple-text", {
-    strings:["precision", "passion", "sustainability in mind", "elegance and comfort", "unmatched quality"],
-    typeSpeed:100,
-    backpeed:100,
-    backDelay: 1000,
-    loop:true
+hamburgerEl.addEventListener('click', () => {
+    navEl.classList.toggle("nav--open")
+    mainEl.classList.toggle('main-content--blurred');
+    body.classList.toggle('no-scroll');
+    hamburgerEl.classList.toggle("hamburger--open")
 
-})
+   
+});
 
+
+navEl.addEventListener('click', () => {
+    navEl.classList.remove("nav--open")
+    hamburgerEl.classList.remove("hamburger--open")
+    body.classList.remove('no-scroll');
+    mainEl.classList.remove('main-content--blurred');
+
+   
+});
