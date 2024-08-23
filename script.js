e4d52b5d347382c15bf24afef11810e6e766f948
ismailou <ismailou@uio.no>
2024-08-24 00:36:20 +0200

    responsive hero section and language dropdown

--- script.js
+++ script.js
@@ -21,3 +21,18 @@ navEl.addEventListener('click', () => {
 
    
 });
+
+
+var typed = new Typed(".multiple-text", {
+    strings:["precision.", "passion.", "sustainability in mind.", "elegance and comfort.", "unmatched quality."],
+    typeSpeed:100,
+    backpeed:100,
+    backDelay: 1000,
+    loop:true
+
+})
+
+
+
+
+
