1ac6288d8100613017c7026f15b10b7b0354dc29
ismailou <ismailou@uio.no>
2024-08-25 17:42:00 +0200

    bootstrap and language support

--- script.js
+++ script.js
@@ -1,38 +1,61 @@
-const navEl = document.querySelector('.nav');
-const hamburgerEl = document.querySelector('.hamburger');
-const mainEl = document.querySelector('.main-content');
-const body = document.querySelector('body');
+document.querySelector('video').playbackRate = 0.8;
 
-hamburgerEl.addEventListener('click', () => {
-    navEl.classList.toggle("nav--open")
-    mainEl.classList.toggle('main-content--blurred');
-    body.classList.toggle('no-scroll');
-    hamburgerEl.classList.toggle("hamburger--open")
+function updateContent(lang, langData) {
+    document.querySelectorAll('[data-i18n]').forEach(element => {
+        const key = element.getAttribute('data-i18n');
+        element.textContent = langData[key];
+    });
 
-   
+    var languageIcon = document.getElementById('languageIcon');
+    if (lang === 'en') {
+        languageIcon.src = 'assets/images/uk-icon.png';
+        languageIcon.alt = 'UK Flag';
+    } else if (lang === 'fr') {
+        languageIcon.src = 'assets/images/france-icon.png';
+        languageIcon.alt = 'France Flag';
+    }
+
+    if (langData.typed_texts) {
+        startTyped(langData.typed_texts);
+    }
+}
+
+function setLanguagePreference(lang) {
+    localStorage.setItem('language', lang);
+}
+
+async function fetchLanguageData(lang) {
+    const response = await fetch(`assets/language/${lang}.json`);
+    return response.json();
+}
+
+// Function to change language
+async function changeLanguage(lang) {
+    setLanguagePreference(lang);
+    const langData = await fetchLanguageData(lang);
+    updateContent(lang, langData);
+
+}
+
+window.addEventListener('DOMContentLoaded', async () => {
+    const userPreferredLanguage = localStorage.getItem('language') || 'en';
+    const langData = await fetchLanguageData(userPreferredLanguage);
+    updateContent(userPreferredLanguage, langData);
 });
 
 
-navEl.addEventListener('click', () => {
-    navEl.classList.remove("nav--open")
-    hamburgerEl.classList.remove("hamburger--open")
-    body.classList.remove('no-scroll');
-    mainEl.classList.remove('main-content--blurred');
+var typed;
 
-   
-});
+function startTyped(strings) {
+    if (typed) {
+        typed.destroy(); // Destroy the previous instance before starting a new one
+    }
 
-
-var typed = new Typed(".multiple-text", {
-    strings:["precision.", "passion.", "sustainability in mind.", "elegance and comfort.", "unmatched quality."],
-    typeSpeed:100,
-    backpeed:100,
-    backDelay: 1000,
-    loop:true
-
-})
-
-
-
-
-
+    typed = new Typed("#multiple-text", {
+        strings: strings,
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    });
+}
